53da5005148d9290f4d7bea394ee6ecc902785aa
tiennguyen <tiennguyenbka198@gmail.com>
2020-06-03 10:36:10 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and do sth! Do it!
         </a>
       </header>
     </div>
